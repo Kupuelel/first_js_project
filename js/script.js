95de2c199d6f5b87688b83bc701b45429e6f038a
Kirill <kupuelel@gmail.com>
2022-04-12 21:01:52 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы посмотрели ?");
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели ?', '');
 // console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {}, 
+    actors: {},
+    genres: [],
+    privat: false
+};
