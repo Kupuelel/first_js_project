f4f30f1fce8df643c16031c52f35b6ae60d0a09b
Kirill <kupuelel@gmail.com>
2022-04-13 19:33:30 +0300

    task if

--- js/script.js
+++ js/script.js
@@ -18,7 +18,13 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i++) {
     const   a = prompt('Один из последних просмотренных фильмов?', ''),
             b = prompt('На сколько оцените его?', '');
-    personalMovieDB.movies[a] = b;
+    
+    if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50 && b.length < 50)  {
+        personalMovieDB.movies[a] = b;
+        console.log('DONE');
+    } else {
+        console.log('ERROR');
+        i--;
+    } 
 }
 
-console.log(personalMovieDB);
